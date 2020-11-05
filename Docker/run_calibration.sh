e2506614b34caa2883676f92e7db62d4216c5599
WinKILLER <and.didenko@gmail.com>
2020-11-05 23:09:13 +0100

    X11 permissions fix

--- Docker/run_calibration.sh
+++ Docker/run_calibration.sh
@@ -4,5 +4,7 @@ if [[ "$(docker images -q mynteye 2> /dev/null)" == "" ]]; then
 	docker build . -t mynteye
 fi
 
+xhost +
+
 docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged -v /dev/bus/usb:/dev/bus/usb mynteye bash
 
